304e7a644aece6dda31ef408d06a7522bc3305c3
jlacancellera <lacancellera.joris@gmail.com>
2020-04-10 17:58:44 +0200

    animation using proper lerp function

--- interact-steve.js
+++ interact-steve.js
@@ -1,19 +1,21 @@
-var canvas = document.getElementById('canvas1');
-var xSize = 200;
-var ySize = 200;
+const canvas = document.getElementById('canvas1');
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, xSize / ySize, 0.1, 1000);
+const xSize = 200;
+const ySize = 200;
+const animationSpeed = 0.08;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, xSize / ySize, 0.1, 1000);
 camera.position.z = 1.5;
 
-var renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
 renderer.setClearColor(0x000000, 0);
 renderer.setSize(xSize, ySize);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material1 = new THREE.MeshStandardMaterial({ color: '#0069d9' });
-var material2 = new THREE.MeshStandardMaterial({ color: '#5a6268' });
-var cube = new THREE.Mesh(geometry, [material1, material1, material1, material2, material2, material2]);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material1 = new THREE.MeshStandardMaterial({ color: '#0069d9' });
+const material2 = new THREE.MeshStandardMaterial({ color: '#5a6268' });
+const cube = new THREE.Mesh(geometry, [material1, material1, material1, material2, material2, material2]);
 scene.add(cube);
 
 const color = 0xFFFFFF;
@@ -22,24 +24,27 @@ const light = new THREE.DirectionalLight(color, intensity);
 light.position.set(-1, 2, 4);
 scene.add(light);
 
-let desiredRx = 0;
-let desiredRy = 0;
+let eulerVector = new THREE.Vector3(0, 0, 0);
+let t = 1;
+
+var rotateY = function () {
+    eulerVector.y += THREE.Math.degToRad(90);
+    t = 0;
+};
 
 var animate = function () {
     requestAnimationFrame(animate);
 
-    if (cube.rotation.x < desiredRx) {
-        cube.rotation.x += 0.05;
+    if (t < 1) {
+        t += animationSpeed;
     }
-    if (cube.rotation.y < desiredRy) {
-        cube.rotation.y += 0.05;
-    }
+
+    cube.rotation.y = THREE.MathUtils.lerp(cube.rotation.y, eulerVector.y, t);
     renderer.render(scene, camera);
 };
 animate();
 
-var onCanvasClick = function (event) {
-    console.log(`Canvas ${event.target} click.`);
-    desiredRy += THREE.Math.degToRad(90);
+var onCanvasClick = function () {
+    rotateY();
 };
-canvas.addEventListener('click', function (event) { onCanvasClick(event); }, false);
+canvas.addEventListener('click', function () { onCanvasClick(); }, false);
