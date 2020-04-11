b7850389b7c3080ce676ec9ead04b143a3cea483
jlacancellera <lacancellera.joris@gmail.com>
2020-04-11 23:52:57 +0200

    Finishing one first interesting version

--- src/multiToggleButton.js
+++ src/multiToggleButton.js
@@ -1,11 +1,25 @@
 import * as THREE from 'three';
 
-export class multiToggleButton{
-    constructor(canvas, xSize, ySize, animationSpeed) {
-        // const canvas = document.getElementById('canvas1');
-        // const xSize = 200;
-        // const ySize = 200;
-        // const animationSpeed = 0.01;
+export class multiToggleButton {
+    constructor(settings) {
+        const canvas = settings.canvas;
+        const xSize = settings.xSize || 200;
+        const ySize = settings.ySize || 200;
+
+        const animationSpeed = settings.animationSpeed || 0.01;
+
+        const states = settings.states || ["On", "Off"];
+        const colors = settings.colors || [];
+        const defaultColor = settings.defaultColor || '#a9a9a9';
+
+        const clickCallback = settings.clickCallback;
+
+        while (colors.length < 6) {
+            colors.push(defaultColor);
+        }
+
+        let stateIndex = 0;
+        let state = states[0];
 
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, xSize / ySize, 0.1, 1000);
@@ -16,9 +30,9 @@ export class multiToggleButton{
         renderer.setSize(xSize, ySize);
 
         const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material1 = new THREE.MeshStandardMaterial({ color: '#0069d9' });
-        const material2 = new THREE.MeshStandardMaterial({ color: '#5a6268' });
-        const cube = new THREE.Mesh(geometry, [material1, material1, material1, material2, material2, material2]);
+        const materials = colors.map(x => new THREE.MeshStandardMaterial({ color: x }))
+
+        const cube = new THREE.Mesh(geometry, materials);
         scene.add(cube);
 
         const color = 0xFFFFFF;
@@ -28,13 +42,30 @@ export class multiToggleButton{
         scene.add(light);
 
         let eulerVector = new THREE.Vector3(0, 0, 0);
-        let t = 1;
+        let t = 0;
 
         var rotate = function () {
-            eulerVector.y += THREE.Math.degToRad(90);
-            // eulerVector.z += THREE.Math.degToRad(45);
+            if (stateIndex == 0) {
+                eulerVector = new THREE.Vector3(0, THREE.Math.degToRad(270), 0);
+            }
+            if (stateIndex == 1) {
+                eulerVector = new THREE.Vector3(0, THREE.Math.degToRad(90), 0);
+            }
+            if (stateIndex == 2) {
+                eulerVector = new THREE.Vector3(THREE.Math.degToRad(90), 0, 0);
+            }
+            if (stateIndex == 3) {
+                eulerVector = new THREE.Vector3(THREE.Math.degToRad(270), 0, 0);
+            }
+            if (stateIndex == 4) {
+                eulerVector = new THREE.Vector3(0, THREE.Math.degToRad(0), 0);
+            }
+            if (stateIndex == 5) {
+                eulerVector = new THREE.Vector3(0, THREE.Math.degToRad(180), 0);
+            }
             t = 0;
         };
+        rotate();
 
         var animate = function () {
             requestAnimationFrame(animate);
@@ -51,8 +82,17 @@ export class multiToggleButton{
         animate();
 
         var onCanvasClick = function () {
+            stateIndex = (stateIndex >= states.length - 1) ? 0 : stateIndex + 1;
+            state = states[stateIndex];
+
             rotate();
+
+            if (clickCallback)
+                clickCallback(canvas, state);
         };
+        if (clickCallback)
+            clickCallback(canvas, state);
+
         canvas.addEventListener('click', function () { onCanvasClick(); }, false);
     }
 }
